2c6bc9f74c3f19812a0fac7cc509decbfe3ec5db
Aditya Shrivastav <shriadishri@gmail.com>
2020-05-28 14:59:19 +0530

    delete button added

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 var makeNewNote = function () {
-  var newPara = document.createElement("p");
-  newPara.setAttribute("class", "newParaClass");
-  var enterText = document.getElementById("enter_text");
+  var cellContainer = document.getElementById("note_row");
 
+  // Bootstrap one cell added
+  var divCell = document.createElement("div");
+  divCell.setAttribute(
+    "class",
+    "note_cell col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3"
+  );
+  cellContainer.appendChild(divCell);
+
+  // Note made (layout)
+  var divNote = document.createElement("div");
+  divNote.setAttribute("class", "note_layout");
+  divCell.appendChild(divNote);
+
+  // Note text
+  var divNoteText = document.createElement("div"); // Note text div
+  divNoteText.setAttribute("class", "note_text");
+  divNote.appendChild(divNoteText);
+
+  var newPara = document.createElement("p"); // Para for note text div
+  divNoteText.appendChild(newPara);
+
+  var enterText = document.getElementById("enter_text"); // Text taken from input to new note
   var newText = document.createTextNode(enterText.innerHTML);
-
   newPara.appendChild(newText);
 
-  var divOne = document.createElement("div");
-  divOne.setAttribute(
-    "class",
-    "note_layout col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4"
-  );
-  var divTwo = document.createElement("div");
-  divTwo.setAttribute("class", "newDivClass");
+  // Note options
+  var divNoteOptions = document.createElement("div"); // Note options div
+  divNoteOptions.setAttribute("class", "note_options");
+  divNote.appendChild(divNoteOptions);
 
-  var divContainer = document.getElementById("note_row");
+  var spanNoteOptions = document.createElement("span"); // Delete note option
+  divNoteOptions.appendChild(spanNoteOptions);
+  var deleteNote = document.createElement("i");
+  deleteNote.setAttribute("class", "fas fa-times");
+  spanNoteOptions.appendChild(deleteNote);
 
-  divContainer.appendChild(divOne);
-  divOne.appendChild(divTwo);
-  divTwo.appendChild(newPara);
+  var removeNote = function () {
+    divNote.remove();
+  };
+  deleteNote.addEventListener("click", removeNote);
 };
